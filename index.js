5787e6ba64b5fe4a30c150e464ef0ce76cfe7fa8
cosmokhairul <porosh.means.touch@gmail.com>
2025-05-19 00:31:23 +0600

    create operation made

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jevmuq3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -31,10 +29,14 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    app.post('/coffees', async(req,res)=>{
-      const newCoffee = req.body
+    const coffeesCollection = client.db("coffeeDB").collection("coffees");
+
+    app.post("/coffees", async (req, res) => {
+      const newCoffee = req.body;
       console.log(newCoffee);
-    })
+      const result = await coffeesCollection.insertOne(newCoffee);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
